9118e55f7f6b27bb49e01d0f99dc5e33454cc5c9
Juan David Espitia <juandespitia14@gmail.com>
2021-03-17 20:56:23 -0500

    paquetes en python

---
+++ scripts/paquetes/paquete/adios/despedidas.py
@@ -0,0 +1,7 @@
+# Este es un modulo con funciones que saluda
+def despedida():
+    print("te estoy despidiendo desde la funcion saludar del modulo despedidas.py")
+
+class Despedida():
+    def __init__(self):
+        print("Hola te estoy despidiendo desde el init de la clase Despedida")
---
+++ scripts/paquetes/paquete/hola/saludos.py
@@ -0,0 +1,7 @@
+# Este es un modulo con funciones que saluda
+def saludar():
+    print("te estoy saludando desde la funcion saludar del modulo saludo.py")
+
+class Saludo():
+    def __init__(self):
+        print("Hola te estoy saludando desde el init de la clase Saludo")
